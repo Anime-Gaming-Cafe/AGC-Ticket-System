d1166f167e5b1e0593eee9a43ae142529f3a0041
Fabi-Chan <fabi@fabi-chan.me>
2023-11-01 08:22:02 +0100

    Reduced e.Interaction calls

--- src/Eventlistener/TicketControlButtonListener.cs
+++ src/Eventlistener/TicketControlButtonListener.cs
@@ -19,63 +19,64 @@ public class TicketManagerEventHandler : BaseCommandModule
     {
         _ = Task.Run(async () =>
         {
-            if (e.Interaction.Data.CustomId == "ticket_claim")
+            string cid = e.Interaction.Data.CustomId;
+            if (cid == "ticket_claim")
             {
                 await TicketManagerHelper.ClaimTicket(e);
             }
-            else if (e.Interaction.Data.CustomId == "ticket_close")
+            else if (cid == "ticket_close")
             {
                 await TicketManager.CloseTicket(e, e.Channel);
             }
-            else if (e.Interaction.Data.CustomId == "ticket_delete")
+            else if (cid == "ticket_delete")
             {
                 await TicketManagerHelper.DeleteTicket(e);
             }
-            else if (e.Interaction.Data.CustomId == "ticket_add_user")
+            else if (cid == "ticket_add_user")
             {
                 await TicketManagerHelper.AddUserToTicketSelector(e.Interaction);
             }
-            else if (e.Interaction.Data.CustomId == "adduser_selector")
+            else if (cid == "adduser_selector")
             {
                 await TicketManagerHelper.AddUserToTicketSelector_Callback(e);
             }
-            else if (e.Interaction.Data.CustomId == "removeuser_selector")
+            else if (cid == "removeuser_selector")
             {
                 await TicketManagerHelper.RemoveUserFromTicketSelector_Callback(e);
             }
-            else if (e.Interaction.Data.CustomId == "ticket_remove_user")
+            else if (cid == "ticket_remove_user")
             {
                 await TicketManagerHelper.RemoveUserFromTicketSelector(e.Interaction);
             }
-            else if (e.Interaction.Data.CustomId == "ticket_more")
+            else if (cid == "ticket_more")
             {
                 await TicketManagerHelper.RenderMore(e);
             }
-            else if (e.Interaction.Data.CustomId == "ticket_userinfo")
+            else if (cid == "ticket_userinfo")
             {
                 await TicketManagerHelper.UserInfo(e.Interaction);
             }
-            else if (e.Interaction.Data.CustomId == "userinfo_selector")
+            else if (cid == "userinfo_selector")
             {
                 await TicketManagerHelper.UserInfo_Callback(e);
             }
-            else if (e.Interaction.Data.CustomId == "ticket_flagtranscript")
+            else if (cid == "ticket_flagtranscript")
             {
                 await TicketManagerHelper.GenerateTranscriptAndFlag(e.Interaction);
             }
-            else if (e.Interaction.Data.CustomId == "transcript_user_selector")
+            else if (cid == "transcript_user_selector")
             {
                 await TicketManagerHelper.TranscriptFlag_Callback(e.Interaction, client);
             }
-            else if (e.Interaction.Data.CustomId == "generatetranscript")
+            else if (cid == "generatetranscript")
             {
                 await TicketManagerHelper.GenerateTranscriptButton(e.Interaction);
             }
-            else if (e.Interaction.Data.CustomId == "ticket_snippets")
+            else if (cid == "ticket_snippets")
             {
                 await TicketManagerHelper.RenderSnippetSelector(e.Interaction);
             }
-            else if (e.Interaction.Data.CustomId.StartsWith("snippet_selector_"))
+            else if (cid.StartsWith("snippet_selector_"))
             {
                 await SnippetManagerHelper.SendSnippetAsync(e.Interaction);
             }
